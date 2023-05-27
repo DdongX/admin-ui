e5fdfb228708b8c22e9d07b7f7cc952dfaa89055
DdongX <2510589885@qq.com>
2023-05-27 23:16:57 +0800

    feature 项目搭建和初始化

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import 'normalize.css'
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
